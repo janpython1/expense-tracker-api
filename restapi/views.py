fdb7e89a3052581347140fabd25cf212560b5709
janpython1 <janyujuan@hotmail.com>
2021-08-27 14:46:11 -0400

    Added IsAuthenticated to views and logged in with username and password.

--- restapi/views.py
+++ restapi/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from restapi import models, serializers
 from rest_framework.generics import RetrieveDestroyAPIView, ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class ExpenseListCreate(ListCreateAPIView):
     serializer_class = serializers.Expense
     queryset = models.Expense.objects.all()
     filterset_fields = ["category", "merchant"]
+    permission_classes = [IsAuthenticated]
 
 
 class ExpenseRetrieveDelete(RetrieveDestroyAPIView):
